8fa2e1a8a933411d67b32e14ce7a9c0b9fd218a0
markmohr15 <mark@interfaceschool.com>
2016-01-20 10:31:56 -0600

    Fix Guardian migration, get sass working

--- db/schema.rb
+++ db/schema.rb
@@ -76,26 +76,23 @@ ActiveRecord::Schema.define(version: 20160116024923) do
   end
 
   create_table "guardians", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email"
+    t.string   "encrypted_password"
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count"
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
     t.string   "avatar_file_name"
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
   end
-
-  add_index "guardians", ["email"], name: "index_guardians_on_email", unique: true, using: :btree
-  add_index "guardians", ["reset_password_token"], name: "index_guardians_on_reset_password_token", unique: true, using: :btree
 
   create_table "memberships", force: :cascade do |t|
     t.integer  "child_id"
