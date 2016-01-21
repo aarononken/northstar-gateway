21dc6e04e63d3a251f260468b6028b0ced85613f
Aaron Onken <aarononken@gmail.com>
2016-01-21 15:52:17 -0600

    Added a bit to guardians_controller.rb. Still a couple of issues, though.

--- app/controllers/guardians_controller.rb
+++ app/controllers/guardians_controller.rb
@@ -1,11 +1,17 @@
-GuardiansController < ApplicationController
+class GuardiansController < ApplicationController
 	def new
 		@guardian = Guardian.new
 	end
 
 	def create
-		@guardian = Guardian.new(guardian.params)
+		@guardian = Guardian.new(guardian_params)
 		@guardian.password = rand(10 ** 5)
+
+		if @guardian.save
+			redirect_to root_url, notice: "Successfully created!"
+		else
+			render :new, alert: "Something was wrong. Try again."
+		end
 	end
 
 	def index
